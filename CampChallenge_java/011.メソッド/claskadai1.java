008afb48227da9c28e5f3cdf15a2ff8cee0f6a98
生田　颯 <>
2018-06-15 03:55:36 +0900

    メソッド（引数）課題１提出します

---
+++ CampChallenge_java/011.メソッド/claskadai1.java
@@ -0,0 +1,22 @@
+
+import jdk.nashorn.internal.parser.TokenType;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author triveransaruku
+ */
+public class claskadai1 {
+    public static void main(String[] args) {
+        
+        add(1);
+    }
+    public static void add(int num1) {
+        System.out.println(num1);
+    }
+}
