30e66f9f66af2799f5c156c798c7ed801e9d79c9
生田　颯 <>
2018-06-15 13:08:10 +0900

    メソッド（引数）課題２提出します

---
+++ CampChallenge_java/011.メソッド/classkadai2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author triveransaruku
+ */
+
+public class classkadai2 {
+    public static void main(String[] args) {
+    
+        add(3, 5, false);
+    
+    }
+    
+    
+    
+    public static void add(int x, int y, boolean z) {
+        
+//引数1,2,3を入れる
+        int ans = x * y;
+        if(z==true){
+            ans*=ans;}
+        System.out.println(ans);
+        
+//引数3の定義を入れる
+    }
+    }
