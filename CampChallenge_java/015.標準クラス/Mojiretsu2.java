865ffed36bd349e972203e6efb823014d937aa97
生田　颯 <>
2018-07-04 16:21:54 +0900

    標準クラス課題６提出します

---
+++ CampChallenge_java/015.標準クラス/Mojiretsu2.java
@@ -0,0 +1,13 @@
+
+//変数を宣言し，自分のメールアドレス（架空のものでも構いません）をその値としてください。
+//メールアドレスの「@」以降の文字数を画面に表示してください。
+
+public class Mojiretsu2 {
+
+    public static void main(String[] args) {
+        String ad = "ikutahayate@softbank.ne.jp";
+        System.out.print(ad.substring(11));
+
+    }
+
+}
